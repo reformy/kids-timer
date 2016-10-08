26ce4b406fc39655de2f03cd9a8a9a058c5eea1d
Yair Ben-Meir <reformy@gmail.com>
2016-10-08 23:08:36 +0300

    Showing seconds! nice.

--- kids-timer-code/kids-timer-code.ino
+++ kids-timer-code/kids-timer-code.ino
@@ -3,14 +3,14 @@
 SevSeg sevseg;
 
 void setup() {
-   byte numDigits = 2;
-   byte digitPins[] = {8, 7};
-   byte segmentPins[] = {5, 9, 10, 12, 11, 6, 4, 0};
-   sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins);
+  byte numDigits = 2;
+  byte digitPins[] = {8, 7};
+  byte segmentPins[] = {5, 9, 10, 12, 11, 6, 4, 14};
+  sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins);
 }
 
 void loop() {
-  sevseg.setNumber(31,0);
+  sevseg.setNumber(millis()/1000,0);
   sevseg.refreshDisplay();
 }
 
